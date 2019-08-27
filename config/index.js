9e5fc0e8bc0afa8fad245d283fe01c3c9a551cbb
lxbuaa2017 <1753312844@qq.com>
2019-08-27 14:41:50 +0800

    bug fix

--- config/index.js
+++ config/index.js
@@ -11,7 +11,6 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-
     },
 
     // Various Dev Server settings
@@ -52,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
