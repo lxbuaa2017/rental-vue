55413804e4f7a3dc269cdaf955e5e02326ba0ced
ForsakenM <z768502753@163.com>
2019-08-31 11:02:21 +0800

    完善工单前端

--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <el-button type="info" @click="SdialogFormVisible=true">报修</el-button>
-    <el-dialog  title="建立新工单" :visible.sync="SdialogFormVisible">
+    <el-button type="info" @click="SdialogFormVisible=true">创建工单</el-button>
+    <el-dialog  title="创建新工单" :visible.sync="SdialogFormVisible">
       <el-form :model="form">
         <el-form-item label="姓名" :label-width="formLabelWidth">
           <el-input v-model="username" autocomplete="off" :disabled="true"></el-input>
         </el-form-item>
-        <el-form-item label="房间地址" :label-width="formLabelWidth">
-          <el-input v-model="newWorkOrder.location" autocomplete="off"></el-input>
+        <el-form-item label="报修内容" :label-width="formLabelWidth">
+          <el-input v-model="newWorkorder.message" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="报修说明" :label-width="formLabelWidth">
-          <el-input v-model="newWorkOrder.message" autocomplete="off"></el-input>
+        <el-form-item label="报修地址" :label-width="formLabelWidth">
+          <el-input v-model="newWorkorder.address" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="上传图片" :label-width="formLabelWidth">
           <br>
@@ -33,137 +33,90 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="SdialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="commitWorkOrder()">确 定</el-button>
+        <el-button type="primary" @click="commitWorkorder()">确 定</el-button>
       </div>
     </el-dialog>
-  <div class="ticket-wrap">
-    <h2 style="padding-left: 10px; text-align: left">待处理工单</h2>
-    <div class="infinite-list-wrapper" style="overflow:auto">
-      <ul
-        class="list"
-        v-infinite-scroll="load"
-        infinite-scroll-disabled="disabled"
-        style="list-style-type: none;padding-inline-start: 0px">
-        <li v-for="(list) in lists" v-bind:key="list.id">
-          <div class="ticketShow">
-            <el-row>
-              <el-card :body-style="{ padding: '0px' }" shadow="hover">
-                <img :src=list.image class="image">
-                <div class="label">
-                  <div style="padding: 5px">
-                    <el-link :underline="false" style="font-size: 16px">{{list.title}}</el-link>
-                    <time class="time">{{list.time}}</time>
+    <div class="order-wrap">
+      <h2 style="padding-left: 10px; text-align: left">未解决工单</h2>
+      <div class="infinite-list-wrapper" style="overflow:auto">
+        <ul
+          class="list"
+          v-infinite-scroll="load"
+          infinite-scroll-disabled="disabled"
+          style="list-style-type: none;padding-inline-start: 0px">
+          <li v-for="workorder in workorders" v-bind:key="workorder.id">
+            <div class="orderWorkorder">
+              <el-row>
+                <el-card :body-style="{ padding: '50px' }" shadow="hover">
+                  <div class="label">
+                    <div style="padding: 5px">
+                      <el-row>
+                        <el-col :span="6">
+                          <time class="time">{{workorder.createdTime}}</time>
+                        </el-col>
+                        <el-col :span="18">
+                          <el-row>
+                            <el-col>
+                              <div>{{workorder.message}}</div>
+                            </el-col >
+                            <el-col v-if="workorder.urls!=null&&workorder.urls.length>0">
+                              <el-row :gutter="10" style="text-align: right;float:right" v-for="url in workorder.urls" v-bind:key="url">
+                                <el-col style="text-align: right;display: inline-block" :span="6">
+                                  <el-image
+                                    style="width: 100px; height: 100px;text-align: right"
+                                    :src="url"
+                                    :preview-src-list="srcList">
+                                  </el-image>
+                                </el-col>
+                              </el-row>
+                            </el-col>
+                          </el-row>
+                        </el-col>
+                      </el-row>
+                    </div>
                   </div>
-                  <div class="position">{{list.address}}</div>
-                  <div class="buttongroup">
-                    <el-button class="button" type="primary" v-on:click="view(list.id)">查看</el-button>
-                    <el-button class="button" type="success" plain v-on:click="append(list.id)">附加</el-button>
-                  </div>
-                </div>
-              </el-card>
-            </el-row>
-          </div>
-        </li>
-      </ul>
-      <p v-if="loading" style="color: #2c3e50">加载中...</p>
-      <p v-if="noMore" style="color: #2c3e50">没有更多了</p>
+                </el-card>
+              </el-row>
+            </div>
+          </li>
+        </ul>
+        <p v-if="loading" style="color: #2c3e50">加载中...</p>
+        <p v-if="noMore" style="color: #2c3e50">没有更多了</p>
+      </div>
     </div>
   </div>
-  </div>
 </template>
-
-<style scoped>
-  .time {
-    font-size: 13px;
-    color: #999;
-    text-align: right;
-    float: right;
-  }
-
-  .position {
-    padding: 5px;
-    font-size: 13px;
-    color: #999;
-  }
-
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
-
-  .buttongroup {
-    margin-top: 15px;
-  }
-
-  .el-button {
-    float: right;
-    margin: 10px;
-    width: auto;
-  }
-
-  .image {
-    height: 120px;
-    display: inline-block;
-    float: left;
-    width: 120px;
-    padding: 15px;
-  }
-
-  .label {
-    padding: 14px;
-    text-align: left;
-    margin-left: 150px;
-  }
-
-  .el-card {
-    height: 150px;
-    margin: 5px;
-  }
-
-</style>
 
 <script>
 import {getCookie} from '../assets/js/cookie.js'
 export default {
+  name: 'Unresolve',
   data () {
     return {
-      name: 'OrderView',
-      id: 1,
-      count: 0,
-      total: 0,
-      loading: false,
+      fileList: [],
+      text: '',
+      username: '',
+      urls: [],
+      dialogImageUrl: '',
       dialogVisible: false,
       SdialogFormVisible: false,
-      currentDate: new Date(),
-      lists: [],
-      workOrder: [],
-      newWorkOrder: {
+      workorders: [],
+      newWorkorder: {
         'message': '',
+        'address': '',
         'tenantUsername': '',
-        'location': '',
         'urls': []
       }
     }
   },
   mounted () {
-    // get total here
-    this.total = 37
     this.username = getCookie('username')
     var self = this
     this.$axios.post('/api/workorder/getbyTenant', this.qs.stringify({
       'username': this.username
     })).then((res) => {
-      self.workOrder = res.data
-      // self.urls = self.complains.urls
+      self.workorders = res.data
     })
-  },
-  computed: {
-    noMore () {
-      return this.count >= this.total
-    },
-    disabled () {
-      return this.loading || this.noMore
-    }
   },
   methods: {
     uploadImgs (file) {
@@ -178,38 +131,12 @@ export default {
         data: param,
         withCredentials: true
       }).then(res => {
-        this.newComplain.urls.push(res.data)
+        this.newWorkorder.urls.push(res.data)
       })
     },
-    load () {
-      this.loading = true
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          if (this.count >= this.total) {
-            break
-          }
-          // get order information here
-          this.lists.push({
-            id: this.id,
-            image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            title: '测试数据' + this.id++,
-            time: '2019年8月25日 10:51',
-            address: '北京市海淀区学院路37号'
-          })
-          this.count++
-        }
-        this.loading = false
-      }, 2000)
-    },
-    view (id) {
-      alert('Clicked \'view\' on room id ' + id)
-    },
-    append (id) {
-      alert('Clicked \'append\' on room id ' + id)
-    },
-    commitWorkOrder () {
-      this.newWorkOrder.tenantUsername = this.username
-      this.$axios.post('/api/workorder/add', this.newWorkOrder)
+    commitWorkorder () {
+      this.newWorkorder.tenantUsername = this.username
+      this.$axios.post('/api/workorder/add', this.newWorkorder)
       this.SdialogFormVisible = false
     },
     handleRemove (file, fileList) {
@@ -222,3 +149,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+</style>
--- src/components/TicketHistory.vue
+++ src/components/TicketHistory.vue
@@ -1,25 +1,46 @@
 <template>
   <div class="order-wrap">
-    <h2 style="padding-left: 10px; text-align: left">历史记录</h2>
+    <h2 style="padding-left: 10px; text-align: left">工单历史记录</h2>
     <div class="infinite-list-wrapper" style="overflow:auto">
       <ul
         class="list"
         v-infinite-scroll="load"
         infinite-scroll-disabled="disabled"
         style="list-style-type: none;padding-inline-start: 0px">
-        <li v-for="(list) in lists" v-bind:key="list.id">
-          <div class="orderShow">
+        <li v-for="workOrder in workOrders" v-bind:key="workOrder.id">
+          <div class="orderComplain">
             <el-row>
-              <el-card :body-style="{ padding: '0px' }" shadow="hover">
-                <img :src=list.image class="image">
+              <el-card :body-style="{ padding: '50px' }" shadow="hover">
                 <div class="label">
                   <div style="padding: 5px">
-                    <el-link :underline="false" style="font-size: 16px">{{list.title}}</el-link>
-                    <time class="time">{{list.time}}</time>
-                  </div>
-                  <div class="position">{{list.address}}</div>
-                  <div class="buttongroup">
-                    <el-button class="button" type="primary" v-on:click="view(list.id)">查看</el-button>
+                    <el-row :gutter="12">
+                      <el-col :span="8">
+                        <time class="time">{{workOrder.createdTime}}</time>
+                      </el-col>
+                      <el-col :span="8">
+                        <el-card shadow="always">
+                          评分：{{workOrder.score}}
+                        </el-card>
+                      </el-col>
+                      <el-col :span="8">
+                        <el-row>
+                          <el-col>
+                            <div>{{workOrder.message}}</div>
+                          </el-col >
+                          <el-col v-if="workOrder.urls!=null&&workOrder.urls.length>0">
+                            <el-row :gutter="10" style="text-align: right;float:right" v-for="url in workOrder.urls" v-bind:key="url">
+                              <el-col style="text-align: right;display: inline-block" :span="6">
+                                <el-image
+                                  style="width: 100px; height: 100px;text-align: right"
+                                  :src="url"
+                                  :preview-src-list="srcList">
+                                </el-image>
+                              </el-col>
+                            </el-row>
+                          </el-col>
+                        </el-row>
+                      </el-col>
+                    </el-row>
                   </div>
                 </div>
               </el-card>
@@ -33,112 +54,30 @@
   </div>
 </template>
 
-<style scoped>
-  .time {
-    font-size: 13px;
-    color: #999;
-    text-align: right;
-    float: right;
-  }
-
-  .position {
-    padding: 5px;
-    font-size: 13px;
-    color: #999;
-  }
-
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
-
-  .buttongroup {
-    margin-top: 15px;
-  }
-
-  .el-button {
-    float: right;
-    margin: 10px;
-    width: auto;
-  }
-
-  .image {
-    height: 120px;
-    display: inline-block;
-    float: left;
-    width: 120px;
-    padding: 15px;
-  }
-
-  .label {
-    padding: 14px;
-    text-align: left;
-    margin-left: 150px;
-  }
-
-  .el-card {
-    height: 150px;
-    margin: 5px;
-  }
-
-</style>
-
 <script>
 import {getCookie} from '../assets/js/cookie.js'
 export default {
+  name: 'Score',
   data () {
     return {
-      name: 'OrderView',
-      id: 1,
-      count: 0,
-      total: 0,
-      loading: false,
-      currentDate: new Date(),
-      lists: []
+      username: '',
+      workOrders: [],
+      score: '5',
+      urls: []
     }
   },
   mounted () {
-    this.total = 37
     this.username = getCookie('username')
     var self = this
-    this.$axios.post('/api/complaints/getRepliedComplaints', this.qs.stringify({
+    this.$axios.post('/api/workorder/getfinish', this.qs.stringify({
       'username': this.username
     })).then((res) => {
-      self.complaints = res.data
+      self.workOrders = res.data
     })
-  },
-  computed: {
-    noMore () {
-      return this.count >= this.total
-    },
-    disabled () {
-      return this.loading || this.noMore
-    }
-  },
-  methods: {
-    load () {
-      this.loading = true
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          if (this.count >= this.total) {
-            break
-          }
-          // get order information here
-          this.lists.push({
-            id: this.id,
-            image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
-            title: '测试数据' + this.id++,
-            time: '2019年8月25日 10:51',
-            address: '北京市海淀区学院路37号'
-          })
-          this.count++
-        }
-        this.loading = false
-      }, 2000)
-    },
-    view (id) {
-      alert('Clicked \'view\' on room id ' + id)
-    }
   }
 }
 </script>
+
+<style scoped>
+
+</style>
